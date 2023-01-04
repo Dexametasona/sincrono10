e3cc1d3f3a2deae096a7aba72df8ad2fe090c928
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-04 09:28:49 -0500

    create comps/modulos/servicio/path/estilo basico

---
+++ src/app/comp-sinc10/comp-sinc10.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Body1Component } from './body1/body1.component';
+import { Body2Component } from './body2/body2.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    Body1Component,
+    Body2Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CompSinc10Module { }
